934718ef084e72722aecbbd90d59783a1becaf4f
watano1168 <watano1168@gmail.com>
2022-01-30 03:31:34 +0900

    コントロ―ラのメソッドを追加

--- server/src/controllers/user.ts
+++ server/src/controllers/user.ts
@@ -2,7 +2,9 @@ import { UserEndpointResponder } from "../endpoints/user";
 import { ClientUserManager } from "../models/managers";
 import { Controller } from "./base";
 import * as EndpointError from "../endpoints/errors";
+import * as Endpoint from "../endpoints/user";
 import { DataAccessFailedError, UserAlreadyRegisteredError } from "../models/errors";
+import { User } from "../models/structures";
 
 export class UserController extends Controller implements UserEndpointResponder {
   async register(clientDiscordId: string) {
@@ -30,5 +32,59 @@ export class UserController extends Controller implements UserEndpointResponder 
       }
       throw error;
     }
+  }
+
+  async config(clientDiscordId: string, params: Endpoint.ConfigParams): Promise<Endpoint.ConfigResult> {
+    const client = await this.getClientUser(clientDiscordId);
+
+    let user;
+    try {
+      user = await client.asUser().updateConfig({
+        enableMention: params.enableMention
+      });
+    } catch (error) {
+      if (error instanceof DataAccessFailedError) {
+        throw new EndpointError.UnavailableError();
+      }
+      throw error;
+    }
+
+    return {
+      discordId: user.discordId,
+      enableMention: user.enableMention
+    };
+  }
+
+  async show(clientDiscordId: string, params: Endpoint.ShowParams): Promise<Endpoint.ShowResult> {
+    const client = await this.getClientUser(clientDiscordId);
+
+    let user: User | undefined;
+    try {
+      user = await client.users.findByDiscordId(params.targetUserDiscordId);
+    } catch (error) {
+      if (error instanceof DataAccessFailedError) {
+        throw new EndpointError.UnavailableError();
+      }
+      throw error;
+    }
+
+    if (!user) {
+      throw new EndpointError.UserNotFoundError({ discordId: params.targetUserDiscordId });
+    }
+
+    let statistics;
+    try {
+      statistics = await user.getStatistics();
+    } catch (error) {
+      if (error instanceof DataAccessFailedError) {
+        throw new EndpointError.UnavailableError();
+      }
+      throw error;
+    }
+
+    return {
+      discordId: user.discordId,
+      ...statistics
+    };
   }
 }
